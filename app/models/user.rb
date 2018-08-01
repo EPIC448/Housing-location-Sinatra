cd1f473f90a0d884ae2d86864cddb0c774d0fb4f
epic0448 <samuelogundiran@Yahoo.com>
2018-08-01 06:41:21 -0400

    refactoryed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   
   validates_presence_of :name, :email, :password
-  validates :name, :emails, :password, uniqueness: true
+  validates :name, :email, uniqueness: true
   has_secure_password
   
     has_many :apartments
